acd28d1d7df31aa7d13438d7930d1d7c38d70391
aashiriftikharr <aashiriftikharr@gmail.com>
2024-01-22 16:44:27 +0500

    refactor: base url globally set

--- src/featrures/housesSlice.js
+++ src/featrures/housesSlice.js
@@ -1,8 +1,9 @@
 import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import { BASE_URL } from "../utils";
 
 // Async thunk for fetching house data
 export const fetchHouses = createAsyncThunk("houses/fetchHouses", async () => {
-  const response = await fetch("https://wizard-world-api.herokuapp.com/houses");
+  const response = await fetch(BASE_URL + "/houses");
   const data = await response.json();
   return data;
 });
